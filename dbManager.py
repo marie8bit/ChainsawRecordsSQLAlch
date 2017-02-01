16320deba6440f8cfb94877b6fbe368835340cce
Marie8bit <yd7581ku@go.minneapolis.edu>
2017-02-01 04:49:38 -0600

    sclalchemy fully implemented

--- dbManager.py
+++ dbManager.py
@@ -1,63 +1,67 @@
-import sqlite3
-import traceback
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from base import Base
+from records import Record
 import ui
 #create and/or connect with the database file
-db = sqlite3.connect('chainsawRecordsDB.db')
-cur = db.cursor()
 #create table and 3 rows of data if it is the first time the application is run
-#
+engine = create_engine('sqlite:///chainsawRecordsDB.db', echo=False)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 def setup():
-    try:
+    save_session = Session()
+    record1 = Record(name = 'Ian Stewart', country = 'Canada', catches = 94)
+    record2 = Record(name = 'Aaron Gregg', country = 'Canada', catches = 88)
+    record3 = Record(name = 'Chad Taylor', country = 'USA', catches = 78)
+    for record in [record1, record2, record3]:
+        if not (getRecord(record.name)):
+            save_session.add(record)
+    save_session.commit()
+    save_session.close()
 
-        cur.execute('create table if not exists records (name text, country text, catches int)')
-        cur.execute('select * from records')
-        data = cur.fetchall()
-        #adds first three rows if the table is empty
-        if (len(data)==0):
-            cur.execute('insert into records values ("Ian Stewart", "Canada", 94)')
-            cur.execute('insert into records values ("Aaron Gregg", "Canada", 88)')
-            cur.execute('insert into records values ("Chad Taylor", "USA", 78)')
-            cur.execute('select * from records')
-            for row in cur:
-                print (row)
-                db.commit()
-        else:
-            for row in cur:
-                print (row)
-    except sqlite3.Error as e:
-        print('rolling back changes because of error:', e)
-        traceback.print_exc()
-        db.rollback()
-    # finally:
-    #     print('closing database')
-    #     db.close()
 def showAll():
-    cur.execute('select * from records')
-    for row in cur:
-        print (row)
+    search_session = Session()
+    # Fetch everything.
+    for record in search_session.query(Record).all():
+        print(record)
+    search_session.close()
 
 def addNewRecord(name, country, catches):
-    cur.execute('insert into records values (?, ?, ?)', (name, country, catches))
-    db.commit()
-    print('New record added to database')
-
-def getRecord(name):
-    cur.execute('select * from records where name = ?', (name,))
-    data = cur.fetchall()
-    if (len(data) != 0):
+    save_session = Session()
+    record = Record(name = name, country = country, catches = catches)
+    if not (getRecord(record.name)):
+        save_session.add(record)
+    save_session.commit()
+    save_session.close()
+#
+def getRecord(aname):
+    search_session = Session()
+    count = search_session.query(Record).filter_by(name = aname).count()
+    if ( count > 0):
+        search_session.close()
         return True
     else:
+        search_session.close()
         return False
 
-def updateRec(name):
-    print ('Enter the new number of catches for '+name)
+def updateRec(aname):
+    update_session = Session()
+    record = update_session.query(Record).filter_by(name = aname)
+    print ('Enter the new number of catches for '+aname)
     stringCa= input()
     catc = ui.getPositiveInt(stringCa)
-    cur.execute('update records set catches = ? where name = ?', (catc, name))
-    db.commit()
+    record.catches = catc
+    update_session.commit()
+    update_session.close()
     print('Record Updated')
 
-def delRecordDB(name):
-    cur.execute('delete from records where name = ?', (name,))
-    db.commit()
+def delRecordDB(aname):
+    delete_session = Session()
+
+    # Query, and delete
+    for record in delete_session.query(Record).filter_by(name = aname):
+        delete_session.delete(record)
+    delete_session.commit()
+    delete_session.close()
     print("Record Deleted")
--- records.py
+++ records.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String
 
+from sqlalchemy.ext.declarative import declarative_base
+
+from base import Base
+class Record(Base):
+    __tablename__ = 'records'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    country = Column(String)
+    catches = Column(Integer)
+
+    def __repr__(self):
+        return 'Record: ID: {} Record Holder = {} Country = {} catches = {}'.format(self.id, self.name, self.country, self.catches)
